d3699f9af429714e68f9c9c9066665f36e509221
SUDARSAN PERUMAL V <sudarsanperumalv@SUDARSANs-MacBook-Air.local>
2022-09-01 18:00:39 +0530

    After change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Header from "./Layouts/Header";
+import Footer from "./Layouts/Footer";
+import Image from "./Layouts/Image";
+import Button from "./Components/Button";
+import Examschedule from "../Pages/Examschedule";
+import Link from 'next/link'
 
-const App = () =>{
-    return (
-        <h1>
-            Hello world! I am using React
-        </h1>
-    )
+
+ const App = () => {
+  return (
+    <div>
+       
+       <Header/>
+       <Image/>
+       <Button/>
+       <Footer/>
+    </div>
+  )
 }
-
 export default App
